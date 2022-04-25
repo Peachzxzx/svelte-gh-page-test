3e8adeae160cbe1feb7110318feffcd21e29ee80
Peachzxzx <peachzxzx@hotmail.com>
2022-04-25 23:39:43 +0700

    github action v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapterStatic from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapterStatic()
 	}
 };
 
